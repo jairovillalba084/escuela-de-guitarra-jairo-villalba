7430494b45e1771760923a4cf50e84597a9c08a4
JairoVL <jairovillalba084@ejemplo.com>
2025-10-03 18:02:30 -0300

    refactor: Organizar estructura de carpetas del proyecto frontend

--- vite-project/src/pages/App.tsx
+++ vite-project/src/pages/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg' // RUTA ACTUALIZADA
 import viteLogo from '/vite.svg'
-import './App.css'
+import '../styles/App.css' // RUTA ACTUALIZADA
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={( ) => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
